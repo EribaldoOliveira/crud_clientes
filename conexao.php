1a111a59509918e6262e656a1034206aa930fefd
EribaldoOliveira <desenvolvedorwebst@gmail.com>
2025-01-06 22:13:46 -0300

    Criação do arquivo Editar clientes - UPDATE

--- conexao.php
+++ conexao.php
@@ -1,12 +1,22 @@
 <?php
 
-$host = 'localhost';
-$db = 'crud_clientes';
+$host = "localhost";
+$db = "crud_clientes";
 $user = "root";
 $pass = "";
 
-$mysqli = new mysqli($host, $user, $pass, $db );
+$mysqli = new mysqli($host, $user, $pass, $db);
+if($mysqli->connect_errno) {
+    die("Falha na conexão com o banco de dados");
+}
 
-    if($mysqli -> connect_errno) {
-        die("Falha na conexão com o banco de dados");
-    }
+function formatar_data($data){
+    return implode('/', array_reverse(explode('-', $data)));
+}
+
+function formatar_telefone($telefone){
+    $ddd = substr ($telefone, 0, 2);
+    $parte1 = substr ($telefone, 2, 5);
+    $parte2 = substr ($telefone, 7);
+    return "($ddd) $parte1-$parte2";
+}
--- editar_cliente.php
+++ editar_cliente.php
@@ -1,13 +1,13 @@
 <?php
 
+include('conexao.php');
+$id = intval($_GET['id']);
 function limpar_texto($str){ 
     return preg_replace("/[^0-9]/", "", $str); 
 }
 
 if(count($_POST) > 0) {
 
-    include('conexao.php');
-    
     $erro = false;
     $nome = $_POST['nome'];
     $email = $_POST['email'];
@@ -39,17 +39,24 @@ if(count($_POST) > 0) {
     if($erro) {
         echo "<p><b>ERRO: $erro</b></p>";
     } else {
-        $sql_code = "INSERT INTO clientes (nome, email, telefone, nascimento, data) 
-        VALUES ('$nome', '$email', '$telefone', '$nascimento', NOW())";
+        $sql_code = "UPDATE clientes
+        SET nome = '$nome', 
+        email = '$email', 
+        telefone = '$telefone',
+        nascimento = '$nascimento'
+        WHERE id = '$id'";
         $deu_certo = $mysqli->query($sql_code) or die($mysqli->error);
         if($deu_certo) {
-            echo "<p><b>Cliente cadastrado com sucesso!!!</b></p>";
+            echo "<p><b>Cliente atualizado com sucesso!!!</b></p>";
             unset($_POST);
         }
     }
 
 }
 
+$sql_cliente = "SELECT * FROM clientes WHERE id = '$id'";
+$query_cliente = $mysqli->query($sql_cliente) or die($mysqli->error);
+$cliente = $query_cliente->fetch_assoc();
 
 ?>
 <!DOCTYPE html>
@@ -64,19 +71,19 @@ if(count($_POST) > 0) {
     <form method="POST" action="">
         <p>
             <label>Nome:</label>
-            <input value="<?php if(isset($_POST['nome'])) echo $_POST['nome']; ?>" name="nome" type="text">
+            <input value="<?php echo $cliente['nome']; ?>" name="nome" type="text">
         </p>
         <p>
             <label>E-mail:</label>
-            <input value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>" name="email" type="text">
+            <input value="<?php echo $cliente['email']; ?>" name="email" type="text">
         </p>
         <p>
             <label>Telefone:</label>
-            <input value="<?php if(isset($_POST['telefone'])) echo $_POST['telefone']; ?>"  placeholder="(11) 98888-8888" name="telefone" type="text">
+            <input value="<?php if(!empty($cliente['telefone'])) echo formatar_telefone($cliente['telefone']); ?>"  placeholder="(11) 98888-8888" name="telefone" type="text">
         </p>
         <p>
             <label>Data de Nascimento:</label>
-            <input value="<?php if(isset($_POST['nascimento'])) echo $_POST['nascimento']; ?>"  name="nascimento" type="text">
+            <input value="<?php if(!empty($cliente['nascimento'])) echo formatar_data($cliente['nascimento']); ?>"  name="nascimento" type="text">
         </p>
         <p>
             <button type="submit">Salvar Cliente</button>
